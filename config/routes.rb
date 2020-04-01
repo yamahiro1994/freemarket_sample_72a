2531de5c85b934d0af29dd890a4fa7852d2c85be
ZackBasrhin <go.my.way.0620@gmail.com>
2020-04-01 19:57:24 +0900

    2020-04-01

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'items#buy'
+  resources :items do
+    member do
+      get 'buy'
+    end
+  end
 end
