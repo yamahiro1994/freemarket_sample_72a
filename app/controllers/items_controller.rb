734eb1c078a7151774afcb64a6aeb09a5b5070c0
ZackBasrhin <go.my.way.0620@gmail.com>
2020-04-06 20:23:38 +0900

    指摘事項修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
 
   def buy
-    @image = Image.find(@item.id).image
+    @image = @item.images[0].image
     @seller = User.find(@item.seller_id)
   end
   
@@ -29,12 +29,8 @@ class ItemsController < ApplicationController
   def show
     @items = Item.all
     @images = @item.images
-    @image = Image.find(@item.id).image
+    @image = @images[0].image
     @seller = User.find(@item.seller_id)
-  end
-
-  def destroy
-    @item.destroy
   end
 
  private
