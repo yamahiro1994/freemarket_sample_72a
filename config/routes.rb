b69d3db69aaeec36efd2b2ad451ae52d9c9bbcec
hotate1000 <ooo.336.sss@gmail.com>
2020-04-06 17:03:43 +0900

    ユーザーページ作成、クレジットカード一覧画面（200406）

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   
   # root 'items#index'
   # root 'items#t-user-credit-detail'
-  # root 'items#t-user-credit-link'
-  root 'items#t-user-logout'
+  root 'items#t-user-credit-link'
+  # root 'items#t-user-logout'
   # root 'items#t-user-show'
   # 商品詳細画面確認用
   resources :items do
