685ac2849dd08e4746e77eb0c15e41e4fe27a55b
hotate1000 <ooo.336.sss@gmail.com>
2020-04-06 16:53:01 +0900

    ユーザーマイページ作成、事前準備（200406）

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
   }
   
-  root 'items#index'
-  # root 'items#show'
+  # root 'items#index'
+  root 'items#t-user-credit-detail'
+  # root 'items#t-user-credit-link'
+  # root 'items#t-user-logout'
+  # root 'items#t-user-show'
   # 商品詳細画面確認用
   resources :items do
     collection do
