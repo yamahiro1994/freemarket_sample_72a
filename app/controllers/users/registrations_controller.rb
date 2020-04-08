7ff371dd56ffaa7361bb1d136128355974965219
ekusuy26 <y.yuu6221@gmail.com>
2020-04-08 17:22:35 +0900

    Minor correction

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,18 +7,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # GET /resource/sign_up
   def new
     @user = User.new
-
-  #   if @user.save
-  #     redirect_to root_path
-  #     flash[:notice] = "ログインしました"
-  #   else
-  #     flash[:alert] = "ログインに失敗しました"
-    # end
   end
 
   def create
-    @user = User.create(user_params)
-    # @user.password_confomation = user.params[:password]
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_path
       flash[:notice] = "新規登録が完了しました"
