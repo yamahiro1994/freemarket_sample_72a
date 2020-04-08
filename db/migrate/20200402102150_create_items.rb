7cec51e6afa20d117cf156318c583c13fd5d6fe8
ekusuy26 <y.yuu6221@gmail.com>
2020-04-08 15:36:21 +0900

    Completion of list display and category display

--- db/migrate/20200402102150_create_items.rb
+++ db/migrate/20200402102150_create_items.rb
@@ -5,9 +5,9 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.text       :content,         null: false
       t.integer    :price,           null: false
       t.string     :status,          null: false
-      t.string     :delivery_method, null: false
+      t.integer     :delivery_method
       t.integer    :prefecture_id,   null: false
-      t.string    :delivery_days,   null: false
+      t.integer    :delivery_days,   null: false
       t.integer    :delivery_charge, null: false
       t.references :category,        foreign_key: true
       t.references :seller,          foreign_key: {to_table: :users}
