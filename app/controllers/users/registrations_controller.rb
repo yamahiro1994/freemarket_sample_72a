606c2da11921afd1f4dbe0821eac54989e6c8a7a
soumataniguchi <staniguchi824@gmail.com>
2020-04-09 12:08:39 +0900

    CSRF対策記述

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,11 +12,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to sign_in_path
     else
       render "new"
     end
   end  
+
   # POST /resource
   # def birthday_join
   #   year = params[:user]["birthday(1i)"]
