07301de3ceab406c0d64ca9d7157601d6c3f8e9f
ekusuy26 <y.yuu6221@gmail.com>
2020-04-11 22:35:57 +0900

    a

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -27,11 +27,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   return birthday
   # end
   
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:nickname,:email,:password,:password_confirmation,:family_name,:family_name_kana,:user_name,:user_name_kana,:birthday, :destination_family_name, :destination_family_name_kana, :destination_name, :destination_name_kana, :postal_code, :prefectures, :municipality, :address, :apartment_room_namber, :phone_number)
-  end
+  # def user_params
+  #   params.require(:user).permit(:nickname,:email,:password,:password_confirmation,:family_name,:family_name_kana,:user_name,:user_name_kana,:birthday, :destination_family_name, :destination_family_name_kana, :destination_name, :destination_name_kana, :postal_code, :prefectures, :municipality, :address, :apartment_room_namber, :phone_number)
+  # end
 
   # def birthday_join
 
