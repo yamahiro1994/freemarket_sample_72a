9f06c62813d87a7860a2a8581434daa18b122bab
yamahiro1994 <y3h5.ja@gmail.com>
2020-04-12 16:20:58 +0900

    カテゴリ表示修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -130,16 +130,14 @@ class ItemsController < ApplicationController
     Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
     end
+
     @category_children_array = []
-    
-    Category.where(ancestry: child_category.ancestry).each do |children|
-      @category_children_array << children
-    end
+    @category_children_array = Category.where(ancestry: child_category.ancestry)
+
     
     @category_grandchildren_array = []
-    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
-      @category_grandchildren_array << grandchildren
-    end
+    @category_grandchild_array = Category.where(ancestry: grandchild_category.ancestry)
+   
     
   end
 
