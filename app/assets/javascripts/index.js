85968b106f75cb7858c807c49fe6ddb8693d7317
ekusuy26 <y.yuu6221@gmail.com>
2020-04-15 01:28:55 +0900

    Category list Fixed JS event firing

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,13 +1,13 @@
-$(function() {
+document.addEventListener("turbolinks:load", function() {
   $(".category-link").hover(function() {
   $("ul.category1").toggle();
   });
   $(".category-link li ul").hide();
   $(".category-link li").hover(function() {
-      $(">ul:not(:animated)", this).stop(true, true).slideDown("fast");
+      $(">ul:not(:animated)", this).stop(true, true).slideDown();
       $(">a", this).addClass("active");
   }, function() {
-      $(">ul:not(:animated)", this).stop(true, true).slideUp("fast");
+      $(">ul:not(:animated)", this).stop(true, true).slideUp();
       $(">a", this).removeClass("active");
   });
 });
