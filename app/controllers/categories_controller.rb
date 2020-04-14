e60ccf40cbef362868a790f67d199b0f00fce152
ekusuy26 <y.yuu6221@gmail.com>
2020-04-14 18:01:55 +0900

    Create each category list page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,15 @@
 class CategoriesController < ApplicationController
-  def index
-  end
 
   def show
-    @items = Item.order(id: :desc)
+    @category = Category.find(params[:id])
+    if params[:id] == @category.root.id.to_s
+      a = @category.indirect_ids
+    elsif  params[:id] == @category.parent.id.to_s
+      a = @category.child.ids
+    else
+      a = params[:id]
+    end
+    @items = Item.order(id: :desc).where(category_id:a)
     @images = Image.includes(:item)
     @parents = Category.where(ancestry: nil)
   end
