a35fcac7aa2e84f0db655c22a54b5fbe9c9d461f
yamahiro1994 <y3h5.ja@gmail.com>
2020-04-16 13:16:55 +0900

    new, show, view修正

--- db/migrate/20200413064100_change_datatype_municipality_of_users.rb
+++ db/migrate/20200413064100_change_datatype_municipality_of_users.rb
@@ -2,7 +2,7 @@ class ChangeDatatypeMunicipalityOfUsers < ActiveRecord::Migration[5.2]
   def up
     change_column :users, :municipality, :string
   end
-  # デプロイできなかった為、下記dwonは適当に作成
+  # ロールバックできなかった為、下記dwonは適当に作成
   def down
     change_column :users, :municipality, :text
   end
