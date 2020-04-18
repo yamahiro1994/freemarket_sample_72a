0187aba8334917019ebbec26ea410b3c8fede639
yamahiro1994 <y3h5.ja@gmail.com>
2020-04-19 07:26:55 +0900

    ログイン前の詳細ページコメント修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions:      'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
   }
 
   devise_scope :user do
